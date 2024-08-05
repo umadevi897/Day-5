46f3b62099381ad19a82bb1fbd5488e8ccbe8c8d
Umadevi_veluduti <101104398+umadevi897@users.noreply.github.com>
2024-08-05 15:49:49 +0530

    Add files via upload
    task 13 string methods and string method document 
    task 14
    task 15

---
+++ string methods class 5.py
@@ -0,0 +1,99 @@
+# class 5 string functions
+
+# sruthi="python"
+# print(sruthi.upper())
+
+# sruthi="PYTHON"
+# print(sruthi.lower())
+
+# james="my fav hero is sampoo"
+# print(james.count("sampoo"))
+
+# james="my fav hero is sampoo"
+# print(james.index("my"))
+
+# james="my fav hero is sampoo"
+# print(james.index("hero"))
+# print(james.find("hero"))
+
+# james="my fav hero is sampoo"
+# print(james.startswith("my")) 
+
+# james="my fav hero is sampoo"
+# print(james.endswith("sampoo"))
+
+# james="my fav hero is sampoo"
+# websites=["abc.com","xyz.in","pythonlife.in"]
+# in_websites=[]
+# for i in websites:
+#     if i.endswith("in"):
+#         in_websites.append(i)
+# print(in_websites)
+
+
+# satya="kiran"
+# print(satya.isalnum())
+
+# alphanumeric combination of integer and alphabets
+
+# satya="kiran,123"
+# print(satya.isalnum())   
+
+# d="this is book"
+# print(len(d))
+
+# counting spaces
+
+# d=" this is book "
+# print(len(d))
+# s=d.strip()
+# print(len(s))
+
+# left and right
+
+# d=" this is book "
+# print(len(d))
+# s=d.strip()
+# print(len(s))
+# s=d.lstrip()
+# print(len(s))
+
+# title
+# f="the colourfull eye"
+# print(f.title()) 
+
+# shaik=" this is my book "
+# v= shaik.replace("is","are")
+# print(v)
+
+# spilt
+
+# d="this is string class"
+# f=d.split()
+# print(f)
+
+
+# d="this is string class"
+# f=d.split()
+# n=[]
+# for i in f:
+#     if i =="is":
+#         n.append(i)
+#     else:
+#         n.append(i)
+#     print(n)
+
+# split
+
+# d="this is string class"
+# c=d.split()
+# print(c)
+
+# join
+
+# d="this is string class"
+# c=d.split()
+# print(c)
+# s=" ".join(d)
+# print(s)
+
---
+++ task 14.py
@@ -0,0 +1,7 @@
+strings = "Generator-Function : with A generator-function is defined like a normal function, but whenever it needs to generate a vulue. it does with the yield keyword rather than return. if the body of a defcontains yield, the function automatically becomes a generator function."
+
+print(strings)
+
+sample = strings.replace("is", "are").replace("with","that")
+
+print(sample)
